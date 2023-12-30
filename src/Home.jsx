9155e96f529242f999cc85563994aa256f54975c
suguru-toyohara <suguru_toyohara@aceof.technology>
2023-12-30 19:18:42 +0900

    適切な名前にrename

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,26 +5,26 @@ import { TweetCard } from "./components/TweetCard.jsx";
 import { useState } from "react";
 import './mainstyle.css';
 
-const useTweetsList = (initItems) => {
+const UseTweetsList = (initItems) => {
     const [Tweets, SetTweets] = useState(initItems);
 
     //Tweetをリストに追加する関数
-    const addTweet = (item) => {
+    const AddTweet = (item) => {
       SetTweets([item, ...Tweets]);
     };
 
-    return [Tweets, addTweet];
+    return [Tweets, AddTweet];
   };
 
 const Home = () => {
-    const [Tweets, addTweet] = useTweetsList([
+    const [Tweets, AddTweet] = UseTweetsList([
         { user: 'Simeji', userId: '@Simeji1993', tweet: '初期のテストツイート' },
       ]);
     return (
         <div id={'main-frame'}>
           <Stack divider={<Divider />}>
             <TwitterLogo />
-            <TweetField inputFunc={addTweet} />
+            <TweetField inputFunc={AddTweet} />
             {Tweets.map((e, i) => (
               <TweetCard key={i} {...e} />
             ))}
